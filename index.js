c7a04099b7891461792f802b02e4823d0b634953
strarsis <strarsis@gmail.com>
2015-09-12 20:42:23 +0200

    Move out string replace all function to separate module. Fix quote handling. Rename main function name.

--- index.js
+++ index.js
@@ -2,29 +2,24 @@
 // https://github.com/Fyrd/caniuse/issues/1746
 // http://tools.ietf.org/html/rfc3986#section-2.2
 
-var escapeRegexp = require('escape-string-regexp');
-
-var globalReplace = function(search, replace, input) { return input.replace(new RegExp(escapeRegexp(search), "g"), replace); }
-var escapeReplace = function(unsafes, input) {
+var strReplaceAll  = require('str-replace-all');
+var replaceEscaped = function(unsafes, input) {
   var output = input;
-  unsafes.forEach(function(unsafe) { output = globalReplace(unsafe, encodeURIComponent(unsafe), output); });
+  unsafes.forEach(function(unsafe) { output = strReplaceAll(unsafe, encodeURIComponent(unsafe), output); });
   return output;
 }
-var inlineEscape  = function(svgStr, surroundingQuotes){
-  if(!surroundingQuotes) surroundingQuotes = 'double'; // default
 
+var inlineUrlescape  = function(svgStr){
   var unsafes = [ ":", "/", "?", "#", "[", "]", "@",
-                  "!", "$", "&", "'", "(", ")",
+                  "!", "$", "&", "(", ")",
                   "*", "+", ",", ";", "="            ];
+  var escapedStr = replaceEscaped(unsafes, svgStr);
 
-  var escaped = escapeReplace(unsafes, svgStr);
+  var escapedStrQuotes = strReplaceAll('"', "'", escaped);
+    // only ' is allowed unencoded in a URL
 
-  var escapedQuotesFixed = escaped;
-    if(surroundingQuotes == 'double') escapedQuotesFixed = globalReplace('"', "'", escaped);
-                                else  escapedQuotesFixed = globalReplace("'", '"', escaped);
-
-    return escapedQuotesFixed;
+  return escapedStrQuotes;
 }
 
 
-module.exports = inlineEscape;
+module.exports = inlineUrlescape;
